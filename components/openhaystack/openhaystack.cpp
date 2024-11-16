6af7b9d1fb68196dee0d9e804ae3a38c2ae4f272
phamcongtheanh <pocotera@gmail.com>
2024-11-16 23:09:13 +0700

    Update openhaystack.cpp

--- components/openhaystack/openhaystack.cpp
+++ components/openhaystack/openhaystack.cpp
@@ -21,10 +21,9 @@ namespace openhaystack {
 
 static const char *const TAG = "openhaystack";
 
-// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 static esp_ble_adv_params_t ble_adv_params = {
     .adv_int_min = 0x0640, // 1s
-    .adv_int_max = 0x0C80, // 2s
+    .adv_int_max =  0x0C80, // 2s
     .adv_type = ADV_TYPE_NONCONN_IND,
     .own_addr_type = BLE_ADDR_TYPE_RANDOM,
     .peer_addr = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
@@ -32,28 +31,6 @@ static esp_ble_adv_params_t ble_adv_params = {
     .channel_map = ADV_CHNL_ALL,
     .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
 };
-
-void OpenHaystack::dump_config() {
-  ESP_LOGCONFIG(TAG, "OpenHaystack:");
-  ESP_LOGCONFIG(TAG,
-                "  Bluetooth MAC: %02X:%02X:%02X:%02X:%02X:%02X",
-                this->random_address_[0],
-                this->random_address_[1],
-                this->random_address_[2],
-                this->random_address_[3],
-                this->random_address_[4],
-                this->random_address_[5]
-  );
-  ESP_LOGCONFIG(TAG,
-                "  Advertising Key (first six digits): %02X %02X %02X %02X %02X %02X",
-                this->advertising_key_[0],
-                this->advertising_key_[1],
-                this->advertising_key_[2],
-                this->advertising_key_[3],
-                this->advertising_key_[4],
-                this->advertising_key_[5]
-  );
-}
 
 void OpenHaystack::setup() {
   ESP_LOGCONFIG(TAG, "Setting up OpenHaystack device...");
@@ -69,33 +46,18 @@ void OpenHaystack::setup() {
   );
 }
 
-float OpenHaystack::get_setup_priority() const { return setup_priority::BLUETOOTH; }
 void OpenHaystack::ble_core_task(void *params) {
   ble_setup();
 
   while (true) {
-    delay(1000);  // NOLINT
+    // Đợi quảng bá BLE xong rồi vào deep sleep
+    vTaskDelay(pdMS_TO_TICKS(60000));  // Wait for 1 minute before waking up
+    esp_deep_sleep_start();  // Vào chế độ deep sleep
   }
 }
 
-void OpenHaystack::set_addr_from_key(esp_bd_addr_t addr, uint8_t *public_key) {
-  addr[0] = public_key[0] | 0b11000000;
-  addr[1] = public_key[1];
-  addr[2] = public_key[2];
-  addr[3] = public_key[3];
-  addr[4] = public_key[4];
-  addr[5] = public_key[5];
-}
-
-void OpenHaystack::set_payload_from_key(uint8_t *payload, uint8_t *public_key) {
-  /* copy last 22 bytes */
-  memcpy(&payload[7], &public_key[6], 22);
-  /* append two bits of public key */
-  payload[29] = public_key[0] >> 6;
-}
-
 void OpenHaystack::ble_setup() {
-  // Initialize non-volatile storage for the bluetooth controller
+  // Khởi tạo bộ nhớ không thay đổi (NVS) cho Bluetooth controller
   esp_err_t err = nvs_flash_init();
   if (err != ESP_OK) {
     ESP_LOGE(TAG, "nvs_flash_init failed: %d", err);
@@ -147,6 +109,9 @@ void OpenHaystack::ble_setup() {
     return;
   }
 
+  // Đặt công suất BLE thấp
+  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, ESP_PWR_LVL_N12); // Công suất -12 dBm
+
   set_addr_from_key(global_openhaystack->random_address_, global_openhaystack->advertising_key_.data());
   set_payload_from_key(global_openhaystack->adv_data_, global_openhaystack->advertising_key_.data());
 
@@ -155,6 +120,7 @@ void OpenHaystack::ble_setup() {
     ESP_LOGE(TAG, "esp_ble_gap_register_callback failed: %d", err);
     return;
   }
+
   err = esp_ble_gap_set_rand_addr(global_openhaystack->random_address_);
   if (err != ESP_OK) {
     ESP_LOGE(TAG, "esp_ble_gap_set_rand_addr failed: %s", esp_err_to_name(err));
